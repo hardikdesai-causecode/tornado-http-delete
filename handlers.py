072a4a7d35afbc1091ab9e1384977d584b7381fc
Hardik Desai <hardik.desai@causecode.com>
2017-09-18 12:58:06 +0530

    Added handler, optional parameter.

--- handlers.py
+++ handlers.py
@@ -36,10 +36,27 @@ class MainHandler(tornado.web.RequestHandler):
         )
         self.write(response)
 
+class OrgHandler(tornado.web.RequestHandler):
+    def get(self,user_uid,org_uid= None):
+
+        response = dict(user_uid=user_uid)
+        if org_uid:
+            response[org_uid] = org_uid
+
+        self.write(response)
+
+    def delete(self,user_uid,org_uid):
+        response = dict(user_uid=user_uid)
+        if org_uid:
+            response[org_uid] = org_uid
+
+        self.write(response)
+
 
 def make_app():
     return tornado.web.Application([
         (r"/", MainHandler),
+        (r"/uid/([-0-9a-fA-F]+)/org/?([-0-9a-fA-F]+)?", OrgHandler),
     ])
 
 if __name__ == "__main__":
--- tests.py
+++ tests.py
@@ -35,7 +35,7 @@ class TestHandler(AsyncHTTPTestCase):
 
     def test_delete_request(self):
         # Example on how to hit a particular handler as DELETE request.
-        # In this example, we want to test the response code,
+        # In this example, we want to test the redirect,
         post_args = {'name': 'Hardik', 'age': '22'}
         response = self.fetch(
             '/',
@@ -44,6 +44,27 @@ class TestHandler(AsyncHTTPTestCase):
             follow_redirects=True)
 
         self.assertEqual(response.code, 200)
+
+class TestOrg(AsyncHTTPTestCase):
+    def get_app(self):
+        return handlers.make_app()
+
+    def test_get_request(self):
+        response = self.fetch(
+            '/uid/{}/org/'.format('12345'),
+            method='GET',
+            follow_redirects=True)
+
+        self.assertEqual(response.code, 200)
+
+    def test_delete_request(self):
+        response = self.fetch(
+            '/uid/{}/org/{}'.format('12345','54321'),
+            method='DELETE',
+            follow_redirects=True)
+
+        self.assertEqual(response.code, 200)
+
 
 if __name__ == "__main__":
     unittest.main()
